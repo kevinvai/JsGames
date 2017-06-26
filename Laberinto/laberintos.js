78f0aaa5147b10f5ccb9b85f351f5d5e470501c6
kevin <kevin@simbo.mx>
2017-06-26 12:36:13 -0700

    script laberintos completo, con integracion al index

--- Laberinto/laberintos.js
+++ Laberinto/laberintos.js
@@ -33,5 +33,83 @@ function drawRectangle(x, y, style) {
     context.fillStyle = style;
     context.fill();
 }
+
+function canMoveTo(destX, destY) {
+    var imgData = context.getImageData(destX, destY, 15, 15);
+    var data = imgData.data;
+    var canMove = 1; // 1 means: the rectangle can move
+    if (destX >= 0 && destX <= mazeWidth - 15 && destY >= 0 && destY <= mazeHeight - 15) { // check whether the rectangle would move inside the bounds of the canvas
+        for (var i = 0; i < 4 * 15 * 15; i += 4) { // look at all pixels
+            if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 0) { // black
+                canMove = 0; // 0 means: the rectangle can't move
+                break;
+            }
+            else if (data[i] === 0 && data[i + 1] === 255 && data[i + 2] === 0) { // lime: #00FF00
+                canMove = 2; // 2 means: the end point is reached
+                break;
+            }
+        }
+    }
+    else {
+        canMove = 0;
+    }
+    return canMove;
+
+    function moveRect(e) {
+        var newX;
+        var newY;
+        var canMove;
+        e = e || window.event;
+        switch (e.keyCode) {
+            case 38:   // arrow up key
+            case 87: // W key
+                newX = currRectX;
+                newY = currRectY - 3;
+                break;
+            case 37: // arrow left key
+            case 65: // A key
+                newX = currRectX - 3;
+                newY = currRectY;
+                break;
+            case 40: // arrow down key
+            case 83: // S key
+                newX = currRectX;
+                newY = currRectY + 3;
+                break;
+            case 39: // arrow right key
+            case 68: // D key
+                newX = currRectX + 3;
+                newY = currRectY;
+                break;
+            default:
+                return;
+        }
+        movingAllowed = canMoveTo(newX, newY);
+        if (movingAllowed === 1) {      // 1 means 'the rectangle can move'
+            drawRectangle(newX, newY, "#0000FF");
+            currRectX = newX;
+            currRectY = newY;
+        }
+        else if (movingAllowed === 2) { // 2 means 'the rectangle reached the end point'
+            clearInterval(intervalVar); // we'll set the timer later in this article
+            makeWhite(0, 0, canvas.width, canvas.height);
+            context.font = "40px Arial";
+            context.fillStyle = "blue";
+            context.textAlign = "center";
+            context.textBaseline = "middle";
+            context.fillText("Congratulations!", canvas.width / 2, canvas.height / 2);
+            window.removeEventListener("keydown", moveRect, true);
+        }
+    }
+
+
+    function makeWhite(x, y, w, h) {
+        context.beginPath();
+        context.rect(x, y, w, h);
+        context.closePath();
+        context.fillStyle = "white";
+        context.fill();
+    }
+}
 drawMazeAndRectangle(425, 3); // { 425, 3 } is the position of the blue rectangle on the canvas
 window.addEventListener("keydown", moveRect, true); // add this at the bottom of your script
